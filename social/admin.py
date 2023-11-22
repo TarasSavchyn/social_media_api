2d4e25a37bd16041aa4c27e0e5da5bccbf5245f4
Taras Savchyn <savik1992@gmail.com>
2023-11-22 22:39:20 +0200

    set admin to social

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from social.models import (
+    Profile,
+    Post,
+    Like,
+    Comment
+)
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
+
--- social/models.py
+++ social/models.py
@@ -7,21 +7,26 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    following = models.ManyToManyField(
-        "self", symmetrical=False, related_name="my_followers"
-    )
-    followers = models.ManyToManyField(
-        "self", symmetrical=False, related_name="my_following"
-    )
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
 
     def __str__(self):
         return f"Profile of {self.user.email}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Like by {self.user.email} on Post {self.post.id}"
 
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(Like, related_name="post_likes")
 
     def __str__(self):
         return f"Post by {self.user.email} at {self.created_at}"
@@ -35,12 +40,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.user.email} on Post {self.post.id}"
-
-
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Like by {self.user.email} on Post {self.post.id}"
