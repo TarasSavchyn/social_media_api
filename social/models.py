ef7dc39046b8dfdc2ae61de1804dc8f56fd43e4b
Taras Savchyn <savik1992@gmail.com>
2023-11-22 21:59:50 +0200

    makemigrations & migrate social models

--- social/models.py
+++ social/models.py
@@ -1,12 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.contrib.auth.models import User
+
+User = get_user_model()
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    following = models.ManyToManyField('self', symmetrical=False, related_name='my_followers')
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='my_following')
 
     def __str__(self):
         return f"Profile of {self.user.email}"
