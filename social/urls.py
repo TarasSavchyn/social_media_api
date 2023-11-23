8ee04edd08745e43de9fd7187b7077ebc1f8e569
Taras Savchyn <savik1992@gmail.com>
2023-11-23 20:54:34 +0200

    delete Comment&Like viewsets & urls

--- social/urls.py
+++ social/urls.py
@@ -1,11 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from social.views import ProfileViewSet, PostViewSet, CommentViewSet, LikeViewSet
+from social.views import ProfileViewSet, PostViewSet
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet)
 router.register(r"posts", PostViewSet)
-router.register(r"comments", CommentViewSet)
-router.register(r"likes", LikeViewSet)
+
 
 urlpatterns = [] + router.urls
 
--- social/views.py
+++ social/views.py
@@ -52,12 +52,3 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-
-class LikeViewSet(viewsets.ModelViewSet):
-    queryset = Like.objects.all()
-    serializer_class = LikeSerializer
