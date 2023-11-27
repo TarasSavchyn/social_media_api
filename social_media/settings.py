b7f67bacd4c9e79c897edccb2a745b59e272a7b6
Taras Savchyn <savik1992@gmail.com>
2023-11-28 00:32:25 +0200

    add path to show swagger

--- social_media/settings.py
+++ social_media/settings.py
@@ -138,10 +138,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
     ],
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+
 
 }
 
