816cbc43dba11573614ae4c15f185c61aa37b287
Taras Savchyn <savik1992@gmail.com>
2023-11-28 00:17:09 +0200

    install drf_spectacular

--- social_media/settings.py
+++ social_media/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "rest_framework.authtoken",
     "user.apps.UserConfig",
     "social.apps.SocialConfig"
@@ -140,6 +141,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication"
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 
 }
 
