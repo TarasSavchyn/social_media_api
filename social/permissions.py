82cdfb72492ff73e6f2799204b7008215d5d60ac
Taras Savchyn <savik1992@gmail.com>
2023-11-30 16:56:59 +0200

    delete redutant logic from IsProfileOwnerOrReadOnly

--- social/permissions.py
+++ social/permissions.py
@@ -1,9 +1,7 @@
 from rest_framework import permissions
 
-
 class IsProfileOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
-            permissions.IsAuthenticated().has_permission(request, view) and
-            (request.method in permissions.SAFE_METHODS or obj.user == request.user)
+            request.method in permissions.SAFE_METHODS or obj.user == request.user
         )
